a5c0130d55556f967df935adf14445f3c64db565
Tejas Kumthekar <tkumthekar@egencia.com>
2018-04-09 22:55:47 -0500

    Add home feed header

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Image, Linking } from 'react-native';
-import { Container, Content, View, Text, Button, Input, Picker } from 'native-base';
+import { Header, Container, Content, View, Text, Button, Input, Item, Icon, Left, Body, Title, Right } from 'native-base';
+import { NavigationActions } from 'react-navigation';
 
 export default class Home extends Component {
     constructor(props) {
@@ -10,18 +11,44 @@ export default class Home extends Component {
     render() {
         return (
             <Container>
-                <Content>
-                    <Image
-                        source={require('./../../assets/thumb-horizontal-logo.png')}
-                    />
-                    
-                    <View>
-                        <Text>
-                            HOME
-                        </Text>
-                    </View>
-
-                </Content>
+                <Header>
+                    <Left>
+                        <Icon name="ios-search" />
+                    </Left>
+                    <Body>
+                        <Title>Home</Title>
+                    </Body>
+                    <Right>
+                        <Button small badge rounded info onPress={() => {
+                                const resetAction = NavigationActions.reset({
+                                    index: 1,
+                                    actions: [
+                                        NavigationActions.navigate({ routeName: 'Travel'}),
+                                        NavigationActions.navigate({ routeName: 'RideStep1'})
+                                    ]
+                                });
+                                this.props.navigation.dispatch(resetAction);
+                            }}>
+                            <Text>
+                                Ride
+                            </Text>
+                        </Button>
+                        <Button small badge rounded info onPress={() => { 
+                                const resetAction = NavigationActions.reset({
+                                    index: 1,
+                                    actions: [
+                                        NavigationActions.navigate({ routeName: 'Travel'}),
+                                        NavigationActions.navigate({ routeName: 'DriveStep1'})
+                                    ]
+                                });
+                                this.props.navigation.dispatch(resetAction);
+                            }}>
+                            <Text>
+                                Drive
+                            </Text>
+                        </Button>
+                    </Right>
+                </Header>
             </Container>
         );
     }
